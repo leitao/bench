6b846a95b538a77b5f674872a208a0119d9d6888
Breno Leitao <brenohl@br.ibm.com>
2017-04-03 15:37:01 -0500

    new changes

--- bench.sh
+++ bench.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-mkdir cpu
-mkdir mem
-mkdir file
+rm -fr cpu mem file
+mkdir cpu mem file
 
-for i in 1 2 4 8 12 24
+MAX_CPU=`ppc64_cpu --cores-on | awk -F= '{print $2}'`
+
+DEBUG="no"
+
+for i in `seq 1 $MAX_CPU`
 do
-	sysbench --cpu-max-prime=20000 --num-threads=$i --test=cpu run > cpu/cpu_result_$i.txt
-	sysbench --num-threads=$i --test=memory run > mem/mem_result_$i.txt
-	sysbench --num-threads=$i --test=fileio  --file-test-mode=rndwr  run > file/file_result_$i.txt
+	if [ "$DEBUG" = "yes" ] ; then
+		echo $i
+	else
+		sysbench --cpu-max-prime=100000 --num-threads=$i --test=cpu run > cpu/cpu_result_$i.txt
+		sysbench --num-threads=$i --test=memory run > mem/mem_result_$i.txt
+		sysbench --num-threads=$i --test=fileio  --file-test-mode=rndwr  run > file/file_result_$i.txt
+	fi
 done
