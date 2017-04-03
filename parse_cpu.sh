4985ccae9d6e215596a722d627899e34a060d5be
Breno Leitao <brneohl@br.ibm.com>
2017-04-03 15:46:05 -0500

    parse CPU results

---
+++ parse_cpu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MAX_CPU=`ppc64_cpu --cores-on | awk -F= '{print $2}'`
+
+echo "Number of threads		Total Time (s)"
+
+for i in `seq 1 $MAX_CPU`
+do
+
+ 	cat cpu/cpu_result_$i.txt | grep 'Number of threads' | awk -n -F: '{printf $2}'
+        cat cpu/cpu_result_$i.txt | grep 'total time:' | awk -F: '{print "\t" $2}'
+done
