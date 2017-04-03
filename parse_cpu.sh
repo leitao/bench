b6ee387cd4a2e669d625736a6ceafcd88fe097f1
Breno Leitao <brneohl@br.ibm.com>
2017-04-03 15:50:03 -0500

    remove S from the number

--- parse_cpu.sh
+++ parse_cpu.sh
@@ -7,5 +7,5 @@ for i in `seq 1 $MAX_CPU`
 do
 
  	cat cpu/cpu_result_$i.txt | grep 'Number of threads' | awk -n -F: '{printf $2}'
-        cat cpu/cpu_result_$i.txt | grep 'total time:' | awk -F: '{print "\t" $2}'
+        cat cpu/cpu_result_$i.txt | grep 'total time:' | awk -F: '{print "\t" $2}' | sed 's/s//g'
 done
